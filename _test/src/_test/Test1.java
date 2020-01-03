620f591a956ced26efff1e0da6cbcf0f92736c30
000 <000@192.168.21.57>
2020-01-03 11:51:23 +0800

    v2

--- _test/src/_test/Test1.java
+++ _test/src/_test/Test1.java
@@ -3,6 +3,7 @@ package _test;
 public class Test1 {
 	public static void main(String[] args) {
 		int a=100;
+		String s = "abc";
 		System.out.println("Hello world!");
 		System.out.println("Hello java!");
 	}
